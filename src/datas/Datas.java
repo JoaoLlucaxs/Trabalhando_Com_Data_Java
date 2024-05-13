71f06cfb2e5250c3c329aa9e1142a5b163cafd8c
JoaoLlucaxs <queirozjoaolucas8@gmail.com>
2024-05-13 14:39:12 -0300

    ..

--- src/datas/Datas.java
+++ src/datas/Datas.java
@@ -4,6 +4,8 @@
  */
 package datas;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -15,13 +17,13 @@ public class Datas {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         // NOTE QUE ESTÃO DEPRECIADAS DEVIDO A MUDANCA PARA CALENDAR
         Date data=new Date();
         
-        System.out.println("DATA EM MILESEGUNDOS " + data.getTime());
+        System.out.println("DATA EM MILESEGUNDOS " + data.getTime()); // RESPOSTA = 1715621340540
         
-        System.out.println("DIA DO MES " + data.getDate());
+        System.out.println("DIA DO MES " + data.getDate()); 
         
         System.out.println("DIA DA SEMANA " + data.getDay());
         
@@ -32,7 +34,17 @@ public class Datas {
         System.out.println("SEGUNDOS " + data.getSeconds());
         
         System.out.println("ANO " + (data.getYear() + 1900)); // COMO BASE O ANO DE 1900
-
+        
+        
+        /*SIMPLE DATA FORMAT - BEM USADA*/
+        SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        System.out.println("Data atual em formata padrão e string " + sdf.format(data));
+        
+        
+        sdf=new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println("Data em formato para banco de dados " + sdf.format(data));
+        
+        System.out.println("Objeto date " + sdf.parse("1987-10-18")); // por ser objeto date pede para adiconar exception
     }
     
 }
